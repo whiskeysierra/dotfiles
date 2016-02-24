17f1f606dcb604545639ba0973412287955bda22
Willi Schönborn <w.schoenborn@gmail.com>
2016-02-24 22:40:55 +0100

    Simplified mai login aliases

--- plugins/mai/mai.plugin.zsh
+++ plugins/mai/mai.plugin.zsh
@@ -3,6 +3,6 @@
 
 if [ -f ~/.config/mai/mai.yaml ]; then
     cat ~/.config/mai/mai.yaml | shyaml keys | while read account; do
-        alias $account:t="mai login ${(q)account} > /dev/null 2>&1";
+        alias $account="mai login $account";
     done
 fi
