651c9711fa5cd3c4a3420d0edd3a3f2ae18ef6e4
Willi Schönborn <w.schoenborn@gmail.com>
2016-02-26 08:56:31 +0100

    Added relog

--- plugins/mai/mai.plugin.zsh
+++ plugins/mai/mai.plugin.zsh
@@ -4,3 +4,13 @@
 mai list -o tsv | sed 1d | cut -d$'\t' -f1 | while read account; do
     alias $account="mai login $account";
 done
+
+function relog() {
+    local account
+
+    account=$(\
+        cat ~/.config/mai/last_update.yaml | \
+        sed -E 's/.*profile: *([^,}]+).*/\1/')
+
+    mai login $account
+}
