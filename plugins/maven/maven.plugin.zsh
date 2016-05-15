45c24c30d57c39b26f1eec6c7ce421876e54f066
Willi Schönborn <w.schoenborn@gmail.com>
2016-05-15 13:07:25 +0200

    Changed multi-settings support from env vars to files+symlinks

--- plugins/maven/maven.plugin.zsh
+++ plugins/maven/maven.plugin.zsh
@@ -5,9 +5,5 @@ if [ -d ~/Applications/maven/bin ]; then
     export PATH=$PATH:$M2_HOME/bin
 fi
 
-export M2_SETTINGS=~/.m2/settings.xml
-
-alias mvn-css='export M2_SETTINGS=~/.dotfiles/.m2/settings-css.xml'
-alias mvn-oss='export M2_SETTINGS=~/.dotfiles/.m2/settings-oss.xml'
-
-alias mvn='mvn --settings $M2_SETTINGS'
+alias mvn-css='ln -sfv ~/.dotfiles/.m2/settings-css.xml ~/.m2/settings.xml'
+alias mvn-oss='ln -sfv ~/.dotfiles/.m2/settings-oss.xml ~/.m2/settings.xml'
