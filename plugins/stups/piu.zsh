59a95858bb831adeb7260e137f8dbd195604d8db
Willi Schönborn <w.schoenborn@gmail.com>
2016-05-19 15:29:00 +0200

    Simplified piu alias

--- plugins/stups/piu.zsh
+++ plugins/stups/piu.zsh
@@ -3,21 +3,8 @@
 
 # TODO this should be free from zalando specifics
 function _piu() {
-    local name domain
-
-    name=$(account)
-
-    domain=$(\
-        mai list -o tsv | \
-        sed 1d | \
-        grep -E "^$name	" | \
-        cut -d$'\t' -f2 | \
-        cut -d' ' -f4 | \
-        grep -oE '[^():]+' | \
-        sed -E 's/^zalando-//')
-        
     # TODO contrary to what the sources may suggest, piu is not honoring the PIU_USER variable
-    ODD_HOST="odd-$AWS_DEFAULT_REGION.$domain.zalan.do" USER=$PIU_USER piu $*
+    ODD_HOST="odd-$AWS_DEFAULT_REGION.$(account).zalan.do" USER=$PIU_USER piu $*
 }
 
 alias piu=_piu
