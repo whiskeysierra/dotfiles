ac1d56b2dedac11529790cde0519cf40565a1c74
Willi Schönborn <w.schoenborn@gmail.com>
2016-06-04 20:29:00 +0200

    Added GPG support to keychain

--- plugins/keychain/keychain.plugin.zsh
+++ plugins/keychain/keychain.plugin.zsh
@@ -1,4 +1,4 @@
-# A plugin that automatically starts and manages keychain (SSH agent)
+# A plugin that automatically starts and manages keychain (SSH agent and GPG agent)
 
-keychain --quiet ~/.ssh/id_personal ~/.ssh/id_work
+keychain --quiet --agents ssh,gpg ~/.ssh/id_personal ~/.ssh/id_work 0B25C552
 source ~/.keychain/$(hostname)-sh
