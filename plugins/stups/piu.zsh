75b28dc29a17454a1455a95a3948ff83c5f285ff
Willi Schönborn <w.schoenborn@gmail.com>
2016-11-03 20:53:40 +0100

    Added new aliases

--- plugins/stups/piu.zsh
+++ plugins/stups/piu.zsh
@@ -4,3 +4,4 @@
 # TODO this should be free from zalando specifics
 # TODO contrary to what the sources may suggest, piu is not honoring the PIU_USER variable
 alias piu='ODD_HOST="odd-$AWS_DEFAULT_REGION.$(account).zalan.do" USER=$PIU_USER piu $*'
+alias gotthard='ODD_HOST="odd-$AWS_DEFAULT_REGION.$(account).zalan.do" USER=$PIU_USER gotthard --reason "DB access" $*'
