08580e061378fc4effbe13496bae471c0b6940f6
Willi Schönborn <w.schoenborn@gmail.com>
2020-06-08 10:24:05 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -ex
 
+brew tap adoptopenjdk/openjdk
+brew tap graalvm/tap
+brew tap homebrew/cask
+brew tap homebrew/cask-drivers
+brew tap homebrew/core
+brew tap itchyny/tap
+
 brew install azure-cli
 brew install itchyny/tap/base58
 brew install bitwarden-cli
@@ -18,7 +25,9 @@ brew install pinentry-mac
 brew install plantuml
 brew install postgresql
 brew install python
+brew install stow
 brew install terraform
+brew install tree
 brew install wget
 brew install youtube-dl
 brew install zsh
@@ -43,3 +52,4 @@ brew cask install tiptoi-manager
 brew cask install tunnelblick
 brew cask install virtualbox
 brew cask install whatsapp
+
